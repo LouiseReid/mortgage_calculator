9144883b2edeaf74a553b2492bcc26f777bd212b
Louise Reid <l_reid@hotmail.co.uk>
2019-03-26 20:36:50 +0000

    monthly form display

--- lib/ui/monthly_cost.dart
+++ lib/ui/monthly_cost.dart
@@ -6,10 +6,67 @@ class Monthly extends StatefulWidget {
 }
 
 class _MonthlyState extends State<Monthly> {
+  var _mortgageAmount = new TextEditingController();
+  var _period = new TextEditingController();
+  var _interestRate = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Monthly'),
+    return ListView(
+      children: <Widget>[
+        Container(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              children: <Widget>[
+                Padding(
+                    padding: const EdgeInsets.only(
+                        left: 16.0, right: 16.0, bottom: 10.0),
+                    child: TextField(
+                        controller: _mortgageAmount,
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          labelText: 'Monthly Payments',
+                          prefix: Text('£'),
+                        ))),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextField(
+                              controller: _interestRate,
+                              keyboardType: TextInputType.number,
+                              decoration: InputDecoration(
+                                  labelText: 'Interest Rate',
+                                  suffixText: '%',
+                                  suffixStyle: TextStyle(fontSize: 13.0))),
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextField(
+                              controller: _period,
+                              keyboardType: TextInputType.number,
+                              decoration: InputDecoration(
+                                  labelText: 'Period',
+                                  suffixText: 'years',
+                                  suffixStyle: TextStyle(fontSize: 13.0))),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+        Container()
+      ],
     );
   }
 }
