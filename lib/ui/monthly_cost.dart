57f416b4b7f8709059ef671e5a1d726c1e47fa9f
Louise Reid <l_reid@hotmail.co.uk>
2019-03-27 08:41:51 +0000

    montly payments complete

--- lib/ui/monthly_cost.dart
+++ lib/ui/monthly_cost.dart
@@ -10,6 +10,8 @@ class _MonthlyState extends State<Monthly> {
   var _mortgageAmount = new TextEditingController();
   var _period = new TextEditingController();
   var _interestRate = new TextEditingController();
+  double _monthlyRepayment = 0.0;
+  double _totalRepayment = 0.0;
 
   @override
   void initState() {
@@ -24,8 +26,14 @@ class _MonthlyState extends State<Monthly> {
     int months = int.parse(_period.text) * 12;
     double monthlyTop = interestRate * pow((1 + interestRate), months);
     double monthlyBottom = pow(1 + interestRate, months) - 1;
-    double monthlyRate = double.parse(_mortgageAmount.text) * (monthlyTop/monthlyBottom);
-    print(monthlyRate);
+    double monthlyRate =
+        double.parse(_mortgageAmount.text) * (monthlyTop / monthlyBottom);
+    double totalPayment = monthlyRate * months;
+
+    setState(() {
+      _monthlyRepayment = monthlyRate;
+      _totalRepayment = totalPayment;
+    });
   }
 
   @override
@@ -34,7 +42,7 @@ class _MonthlyState extends State<Monthly> {
       children: <Widget>[
         Container(
           child: Padding(
-            padding: const EdgeInsets.all(15.0),
+            padding: const EdgeInsets.fromLTRB(15.0, 50.0, 15.0, 0.0),
             child: Column(
               children: <Widget>[
                 Padding(
@@ -44,7 +52,7 @@ class _MonthlyState extends State<Monthly> {
                         controller: _mortgageAmount,
                         keyboardType: TextInputType.number,
                         decoration: InputDecoration(
-                          labelText: 'Monthly Payments',
+                          labelText: 'Mortgage Amount',
                           prefix: Text('£'),
                         ))),
                 Padding(
@@ -83,7 +91,33 @@ class _MonthlyState extends State<Monthly> {
             ),
           ),
         ),
-        Container()
+        Padding(
+          padding: const EdgeInsets.only(top: 50.0),
+          child: Container(
+            margin: const EdgeInsets.only(left: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "Total Cost of Mortgage: £${_totalRepayment.round()}",
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w100),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  child: Text("Monthly Payments: £${_monthlyRepayment.round()}",
+                      style: TextStyle(
+                          fontSize: 20.0, fontWeight: FontWeight.w500)),
+                )
+              ],
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 38.0),
+          child: Container(
+            child: Icon(Icons.home, color: Colors.deepPurple[400], size: 80.0),
+          ),
+        )
       ],
     );
   }
